ac07137169f0c06867782b4030ea1f68550c7a94
Cuke7 <cuke.dev@mail.com>
2022-08-28 16:25:49 +0200

    test PWA official module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,14 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
+    buildModules: [
+        '@nuxtjs/pwa',
+    ],
+    pwa: {
+        manifest: {
+            name: 'Scriptae',
+            lang: 'fr',
+        }
+    }
 })
 
