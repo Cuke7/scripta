93994e358b2de607010d8fa7fa1fef658c374c03
Cuke7 <cuke.dev@mail.com>
2022-08-28 16:14:05 +0200

    PWA test2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', '@kevinmarrec/nuxt-pwa']
+    modules: ['@nuxtjs/tailwindcss', '@kevinmarrec/nuxt-pwa'],
+    pwa: {
+        manifest: {
+            name: 'My Awesome App',
+            lang: 'fr',
+            useWebmanifestExtension: false
+        }
+    }
 })
 
